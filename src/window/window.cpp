12a387332ef9fd3f4be340d57186109bdef5aeb8
okayscylla <75046957+okayscylla@users.noreply.github.com>
2026-09-26 14:38:04 -0400

    :3 (fix buffer flushing after log message)

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -16,14 +16,14 @@ void KovWindow::init(int w, int h, std::string title, int t_fpss) {
     _sdlwindow = SDL_CreateWindow(title.c_str(), width, height, 0);
 
     _sdlsfmts = SDL_GPU_SHADERFORMAT_MSL | SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_MSL;
-    klog("using shaderformat {}", _sdlsfmts);
+    klog("using shaderformat {}\n", _sdlsfmts);
 
     _sdlhgpu = SDL_CreateGPUDevice(
         _sdlsfmts,
         false,
         NULL
     );
-    klog("bound to gpu device");
+    klog("bound to gpu device\n");
 
     SDL_ClaimWindowForGPUDevice(_sdlhgpu, _sdlwindow);
     SDL_SetGPUSwapchainParameters(
@@ -44,7 +44,7 @@ void KovWindow::setTitle(std::string title) {
     window_title = title;
 
     SDL_SetWindowTitle(_sdlwindow, window_title.c_str());
-    klog("new title {}", title);
+    klog("new title {}\n", title);
 }
 
 void KovWindow::resize(int w, int h) {
@@ -53,12 +53,12 @@ void KovWindow::resize(int w, int h) {
     SDL_SetWindowSize(_sdlwindow, width, height);
 
     renderer->init(_sdlhgpu, _sdlwindow, width, height);
-    klog("new resolution {}x{}", w, h);
+    klog("new resolution {}x{}\n", w, h);
 }
 
 void KovWindow::setTargetFPS(int fps) {
     _target_ft = 1000000000LL / fps;
-    klog("target frametime {}", _target_ft);
+    klog("target frametime {}\n", _target_ft);
 }
 
 void KovWindow::poll() {
@@ -75,13 +75,13 @@ void KovWindow::poll() {
 void KovWindow::setRenderer(Renderer* new_renderer) {
     if (renderer != nullptr) {
         renderer->cleanup();
-        klog("destroyed renderer");
+        klog("destroyed renderer\n");
         delete renderer;
     }
 
     renderer = new_renderer;
     renderer->init(_sdlhgpu, _sdlwindow, width, height);
-    klog("created new renderer");
+    klog("created new renderer\n");
 }
 
 void KovWindow::draw() {
@@ -93,7 +93,7 @@ void KovWindow::draw() {
 void KovWindow::gracefulExit() {
     if (renderer != nullptr) {
         renderer->cleanup();
-        klog("destroyed renderer");;
+        klog("destroyed renderer\n");;
         delete renderer;
     }
 
@@ -105,7 +105,7 @@ void KovWindow::gracefulExit() {
     
     delete _sdlevent;
 
-    klog("cleanup finished, exiting");
+    klog("cleanup finished, exiting\n");
 
     _quit = true;
 }

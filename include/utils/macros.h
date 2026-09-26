903402082e4819b9a87ff33b2b68f41d8199147c
okayscylla <75046957+okayscylla@users.noreply.github.com>
2026-09-26 16:37:00 -0400

    :3 (flipped a sign)

--- include/utils/macros.h
+++ include/utils/macros.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define assertEqual(i, j)           (std::fabs((i) - (j)) < EPSILON)
-#define notZero(i)                  (std::fabs(i) < EPSILON)
+#define notZero(i)                  (std::fabs(i) > EPSILON)
 #define klog(...)                   (std::print("KOV: " __VA_ARGS__))
